5743e1faeb372e89801efd326349a26e60c60c11
Aleksej Komnenović <aleksej.komnenovic@comtrade.com>
2022-06-13 11:43:56 +0200

    add testing class

---
+++ src/main/java/org/testing/AdditionTest.java
@@ -0,0 +1,13 @@
+package org.testing;
+
+import org.junit.*;
+
+public class AdditionTest {
+    AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+
+    @Test
+    public void testAdditionOfIntegers() {
+
+    }
+
+}
