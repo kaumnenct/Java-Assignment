cbd7eb056a7f0e1912a68588745d2b5f11596cb0
Aleksej Komnenović <aleksej.komnenovic@comtrade.com>
2022-06-13 11:31:55 +0200

    instantiate object and test methods for addition

---
+++ src/main/java/org/testing/Main.java
@@ -0,0 +1,11 @@
+package org.testing;
+
+public class Main {
+    public static void main(String[] args) {
+        AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+
+        System.out.println(addTwoNumbers.TwoIntegers(1, 3));
+        System.out.println(addTwoNumbers.TwoDoubles(1.2, 1.5));
+        System.out.println(addTwoNumbers.DoubleAndInt(1.5, 2));
+    }
+}
