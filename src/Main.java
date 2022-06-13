a698ef430418650267b2095d90a1bd0a5de4f90d
Aleksej Komnenović <aleksej.komnenovic@comtrade.com>
2022-06-13 10:44:35 +0200

    add NullPointerException logic and test it inside main func

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+        NPException npexception = new NPException();
+        try {
+            npexception.testNullPointer();
+        }
+        catch (NullPointerException e)
+        {
+            System.out.println("Exception found: \n" + e);
+        }
     }
 }
--- src/NPException.java
+++ src/NPException.java
@@ -1,2 +1,9 @@
 public class NPException {
+    public void testNullPointer(){
+        Integer number = null;
+
+        if (number > 0) {
+            System.out.println(number + "is positive!");
+        }
+    }
 }
