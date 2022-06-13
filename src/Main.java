efdb6ca1edb986afec403b6f40fccd5fceda8252
Aleksej Komnenović <aleksej.komnenovic@comtrade.com>
2022-06-13 10:47:44 +0200

    add method in main function and test exception

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,25 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
         NPException npexception = new NPException();
+        AIOBException aiobException = new AIOBException();
+
+        //testing NullPointerException
         try {
             npexception.testNullPointer();
         }
         catch (NullPointerException e)
         {
-            System.out.println("Exception found: \n" + e);
+            System.out.println("Exception 1. found: \n" + e);
+        }
+
+        //testing ArrayIndexOutOfBoundsException
+        try
+        {
+            aiobException.testArrayIndexOutOfBounds();
+        }
+        catch (IndexOutOfBoundsException e)
+        {
+            System.out.println("Exception 2. found: \n" + e);
         }
     }
 }
