5d303adaf02a932543df6250e8b828e29d5d9bb5
Aleksej Komnenović <aleksej.komnenovic@comtrade.com>
2022-06-13 11:24:17 +0200

    change package file to exceptions

--- src/main/java/org/exceptions/Main.java
+++ src/main/java/org/exceptions/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.exceptions;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/org/exceptions/NPException.java
@@ -0,0 +1,12 @@
+package org.exceptions;
+
+public class NPException {
+    public void testNullPointer(){
+        Integer number = null;
+
+        if (number > 0) {
+            System.out.println(number + "is positive!");
+        }
+    }
+}
+
