e16d39b29ea79f4fb3cf38d5d8d1d41a1d9f5149
Aleksej Komnenović <aleksej.komnenovic@comtrade.com>
2022-06-13 11:31:36 +0200

    add class for number addition
    it adds either two integers, double and integer or two double numbers

---
+++ src/main/java/org/testing/AddTwoNumbers.java
@@ -0,0 +1,16 @@
+package org.testing;
+
+public class AddTwoNumbers {
+    public int TwoIntegers(int a, int b) {
+        return a + b;
+    }
+
+    public double TwoDoubles(double a, double b) {
+        return a + b;
+    }
+
+    public double DoubleAndInt(double a, int b)
+    {
+        return a + b;
+    }
+}
