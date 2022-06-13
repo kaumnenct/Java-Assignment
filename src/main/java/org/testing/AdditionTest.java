68ab45de0b7114c64c00db28cd6663ebe8bb1808
Aleksej Komnenović <aleksej.komnenovic@comtrade.com>
2022-06-13 11:57:31 +0200

    add testing methods for all 3 cases

--- src/main/java/org/testing/AdditionTest.java
+++ src/main/java/org/testing/AdditionTest.java
@@ -1,13 +1,22 @@
 package org.testing;
 
-import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class AdditionTest {
     AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
 
     @Test
     public void testAdditionOfIntegers() {
-
+        assertEquals(addTwoNumbers.TwoIntegers(1, 2), 3);
     }
 
+    @Test
+    public void testAdditionOfDoubles() {
+        assertEquals(addTwoNumbers.TwoDoubles(1.2, 1.5), 2.7, 0.1);
+    }
+    @Test
+    public void testAdditionOfDoubleAndInt() {
+        assertEquals(addTwoNumbers.DoubleAndInt(1.5, 2), 3.5, 0.1);
+    }
 }
