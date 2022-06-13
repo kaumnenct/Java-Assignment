f0a8b4603aa41606a162be68ee07206c25b120c6
Aleksej Komnenović <aleksej.komnenovic@comtrade.com>
2022-06-13 11:20:10 +0200

    change project type to maven 1/2

--- src/NPException.java
+++
@@ -1,9 +0,0 @@
-public class NPException {
-    public void testNullPointer(){
-        Integer number = null;
-
-        if (number > 0) {
-            System.out.println(number + "is positive!");
-        }
-    }
-}
