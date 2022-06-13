a233bafdf66fcbdf7130499078352ca0e85bab02
Aleksej Komnenović <aleksej.komnenovic@comtrade.com>
2022-06-13 10:44:58 +0200

    add ArrayIndexOutOfBoundsException logic

--- src/AIOBException.java
+++ src/AIOBException.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AIOBException {
+    public int testArray() {
+        List numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(5);
+
+        return (int) numbers.get(5);
+    }
 }
